19a9c7288c37dd1bc636a50251000a66d0146bd8
nisha1234 <nnagaraj@purdue.edu>
2015-03-24 18:46:45 -0400

    added parse files

--- Recruitment/Recruitment-Bridging-Header.h
+++ Recruitment/Recruitment-Bridging-Header.h
@@ -1,4 +1,15 @@
 //
-//  Use this file to import your target's public headers that you would like to expose to Swift.
+//  Recruitment-Bridging-Header.h
+//  Recruitment
+//
+//  Created by Stephanie Kyler on 3/24/15.
+//  Copyright (c) 2015 AppAtUs. All rights reserved.
 //
 
+#import "AppDelegate.h"
+
+#ifndef Recruitment_Recruitment_Bridging_Header_h
+#define Recruitment_Recruitment_Bridging_Header_h
+
+
+#endif
